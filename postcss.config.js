d31e9601c38d05d162ce9f2658c18f120ce27593
yuchao.shen <yuchao.shen@visitshanghai.net>
2021-03-30 18:54:08 +0800

    1px问题解决

--- postcss.config.js
+++ postcss.config.js
@@ -5,10 +5,10 @@ module.exports = {
             unitToConvert: 'px',    // 需要转换的单位
             viewportWidth: 750,     // 设计图视口宽度
             unitPrecision: 5,       // 小数保留位数
-            propList: ['*'],        // 支持转换的css属性名称
+            propList: ["*"],        // 支持转换的css属性名称
             viewportUnit: 'vw',     // 转为为vw宽度单位
             fontViewportUnit: 'vw', // 字体单位
-            selectorBlackList: [],  // 选择器黑名单,转换时不会转换符合此处的选择器
+            selectorBlackList: [/^\.ingore_/g],  // 选择器黑名单,转换时不会转换符合此处的选择器
             minPixelValue: 1,       // 最小的转换数值
             mediaQuery: false,      // 媒体查询里面的是否转换
             replace: true,          // 是否直接替换属性值而不是添加备用属性
