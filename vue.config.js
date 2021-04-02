08dc10860afd194ff7e460dd5c6407e45221cb96
yuchao.shen <yuchao.shen@visitshanghai.net>
2021-04-02 18:14:11 +0800

    多页项目集成配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,28 @@
 const path = require('path');
+console.log(123);
 module.exports = {
+    // 配置多页应用
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'src/main.js',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'Index Page',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        introduce: {
+            entry: 'src/introduce.js',
+            template: 'public/introduce.html',
+            filename: 'introduce.html'
+        }
+    },
     chainWebpack: config => {
         const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
         types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
