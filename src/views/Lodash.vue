b04b8709c2ef62223c83e7a658b5f287c46943fa
yuchao.shen <yuchao.shen@visitshanghai.net>
2021-04-02 17:16:04 +0800

    项目优化,包体积优化

---
+++ src/views/Lodash.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+	use lodash.js
+</div>
+</template>
+
+<script>
+import _ from 'lodash';
+export default {
+	name: "Lodash",
+	data() {
+		return {}
+	},
+	mounted() {
+		console.log('use lodash.js page');
+		console.log('lodash.js object', _.fill);
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,13 @@ module.exports = {
     chainWebpack: config => {
         const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
         types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
+        if (process.env.NODE_ENV === 'production') {
+            config.set('externals', {
+                vue: 'Vue',
+                'vue-router': 'VueRouter',
+                'lodash': '_'
+            });
+        }
     },
 }
 
