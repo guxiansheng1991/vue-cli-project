b29902e9c80ad10a386363ae693ba18d0f9a5152
yuchao.shen <yuchao.shen@visitshanghai.net>
2021-04-07 15:58:41 +0800

    一个多页面的项目,此项目中只包含一个项目

--- common.config.js
+++ common.config.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
 const join = require('path').join;
 
+const fileList = [];
+const VIEW_PATH = 'src/views';
 // 读取文件夹
 function readDirs(path) {
     let dirList = fs.readdirSync(path);
     dirList.forEach((item, index) => {
-        console.log(`path: ${path}${item}`);
         readFilesOfDir(path, item);
     });
 }
@@ -16,9 +17,69 @@ function readFilesOfDir(path, dirName) {
     const stat = fs.statSync(dirPath);
     // 读取文件夹下的所有文件, 检查是否存在和文件夹名称相同的三个文件(dirName.html, dirName.vue, dirName.js)
     if (stat.isDirectory()) {
-        const files = fs.readdirSync(path);
-        console.log(files);
+        const files = fs.readdirSync(dirPath);
+        const filesObj = array2obeject(files, dirName);
+        fileList.push(filesObj);
     }
 }
 
-readDirs('src/views/');
+// 验证文件夹文件正确性
+function vertify(list) {
+    let flag = true;
+    for (let i = 0, iLen = list.length; i < iLen; i++) {
+        const item = list[i];
+        const necessaryFiles = [`${item.dirName}.html`, `${item.dirName}.js`, `${item.dirName}.vue`];
+        for (let j = 0, jLen = necessaryFiles.length; j < jLen; j++) {
+            const subItem = necessaryFiles[j];
+            if (!item.hasOwnProperty(subItem)) {
+                console.error(`-------------------${item.dirName}缺少文件${subItem}, 请修改!-------------------`);
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            console.log(`${item.dirName}文件夹文件结构验证通过!`);
+        }
+        if (!flag) {
+            break;
+        }
+    }
+    return flag;
+}
+
+// 数组转为对象
+function array2obeject(list, dirName) {
+    const obj = {};
+    for (const key in list) {
+        obj[list[key]] = list[key];
+    }
+    obj.dirName = dirName;
+    return obj;
+}
+
+// 生成pages
+function createPageParams() {
+    readDirs(VIEW_PATH);
+    const flag = vertify(fileList);
+    let pages = {};
+    if (flag) {
+        fileList.forEach(ele => {
+            const dirName = ele.dirName;
+            pages[dirName] = {
+                // page 的入口
+                entry: `${VIEW_PATH}/${dirName}/${dirName}.js`,
+                // 模板来源
+                template: `${VIEW_PATH}/${dirName}/${dirName}.html`,
+                // 在 dist/home.html 的输出
+                filename: `${dirName}.html`
+            }
+        });
+    }
+    return pages;
+}
+
+// 生成pages变量
+module.exports = {
+    createPageParams: createPageParams
+}
+
--- vue.config.js
+++ vue.config.js
@@ -1,35 +1,11 @@
 const path = require('path');
+const common = require('./common.config');
+const pages = common.createPageParams();
 
 console.log('当前环境变量NODE_ENV:', process.env.NODE_ENV);
 
 module.exports = {
-    // 配置多页应用
-    pages: {
-        index: {
-            // page 的入口
-            entry: 'src/views/home/home.js',
-            // 模板来源
-            template: 'src/views/home/home.html',
-            // 在 dist/home.html 的输出
-            filename: 'home.html',
-            // 当使用 title 选项时，
-            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-            //title: 'home Page',
-            // 在这个页面中包含的块，默认情况下会包含
-            // 提取出来的通用 chunk 和 vendor chunk。
-            //chunks: ['chunk-vendors', 'chunk-common', 'index']
-        },
-        introduce: {
-            entry: 'src/views/introduce/introduce.js',
-            template: 'src/views/introduce/introduce.html',
-            filename: 'introduce.html'
-        },
-        about: {
-            entry: 'src/views/about/about.js',
-            template: 'src/views/about/about.html',
-            filename: 'about.html'
-        }
-    },
+    pages: pages,
     chainWebpack: config => {
         const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
         types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
